055bf152b345cab6d428abd159bb9ba900cc927e
zoo531327 <3131613712@qq.com>
2026-06-27 23:35:53 +0800

    链表（获取尾部节点+尾插法）

---
+++ xian xing biao/lian biao/获取尾部节点+尾插法.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef int elemtype;
+
+//定义顺序表
+typedef struct node{
+    elemtype data;
+    struct node *next;
+}Node;
+
+//初始化
+Node *initlist(){
+    Node *head = (Node*)malloc(sizeof(Node));
+    head->data = 0;
+    head->next = NULL;
+    return head;
+}
+
+//头插法
+int inserthead(Node *L,elemtype e){
+    Node *p = (Node*)malloc(sizeof(Node));
+    p->data = e;
+    p->next = L->next;
+    L->next = p;
+    return 1;
+}
+
+//遍历
+void listnode(Node *L){
+    Node *p = L->next;
+    while(p != NULL){
+        printf("%d ",p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+//获取尾部结点
+Node *get_tail(Node *L){
+    Node *p = L;
+    while(p->next != NULL){
+        p = p->next;
+    }
+    return p;
+}
+
+//尾插法
+Node *inserttail(Node *tail, elemtype e){
+    Node *p = (Node*)malloc(sizeof(Node));
+    p->data = e;
+    tail->next = p;
+    p->next = NULL;
+    return p;
+}
+
+//主函数
+int main(){
+    Node *list = initlist();
+    Node *tail = get_tail(list);
+    inserthead(list,10);
+    inserthead(list,20);
+    inserthead(list,30);
+    listnode(list);
+    return 0;
+}
